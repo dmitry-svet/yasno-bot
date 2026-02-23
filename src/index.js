378f9529a03c0eed871dea82bf20372a07cc3b12
Dmitry Svet <dmitry.svet@togethernetworks.com>
2026-02-23 08:26:44 +0200

    Add startup logging for data dir and user count

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 require('dotenv').config();
 const { startBot } = require('./bot');
 const { startCron } = require('./cron');
+const { getUsers } = require('./store');
 
 const bot = startBot();
 startCron(bot);
 
-console.log('yasno-bot started');
+const dataDir = process.env.DATA_DIR || './data';
+const users = getUsers();
+const userCount = Object.keys(users).length;
+console.log(`yasno-bot started | DATA_DIR=${dataDir} | users: ${userCount}`);
